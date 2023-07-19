e2bdd6a0fe1a4bebe79d1d6e7bbf8daed23b1e63
Posccis <lkzq33@gmail.com>
2023-07-19 01:41:58 -0300

    add(main)="Criada a classe de contexto para fazer o mapeamento e criação das tabelas"

---
+++ FlightBookingLib/Contexto/FlightBookingContext.cs
@@ -0,0 +1,40 @@
+﻿using FlightBookingLib.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Lib.Contexto
+{
+    public class FlightBookingContext : DbContext
+    {
+
+        //protected IConfiguration _config;
+        //protected string _strCnx;
+
+
+        //public FlightBookingContext(IConfiguration config)
+        //{
+
+        //    _config = new ConfigurationManager().AddJsonFile("appsettings.json").Build();
+            
+        //    _strCnx = _config.GetConnectionString("FlightBookingStr");
+        //}
+
+        public DbSet<Passageiro> Passageiros { get; set; }
+        public DbSet<Tripulante> Tripulantes { get; set; }
+        public DbSet<Voo> Voos { get; set; }
+        public DbSet<CartaoDeCredito> CartoesDeCredito { get; set; }
+        public DbSet<Aviao> Avioes { get; set; }
+        public DbSet<Piloto> Pilotos { get; set; }
+        public DbSet<Copiloto> Copiloto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=localhost;Database=FlightBooking;User Id=sa;Password=JK64victorjk;trusted_connection=true;encrypt=false;");
+
+
+    }
+}
