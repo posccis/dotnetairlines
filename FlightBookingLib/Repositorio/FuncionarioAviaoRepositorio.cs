1e1ac1cd3ad036813540510e7bbcfcb244b8a084
Posccis <lkzq33@gmail.com>
2023-07-20 02:05:20 -0300

    add(main)="Criei o repositorio dos funcionarios do avião e criei o construtor dele"

---
+++ FlightBookingLib/Repositorio/FuncionarioAviaoRepositorio.cs
@@ -0,0 +1,66 @@
+﻿using FlightBooking.Lib.Contexto;
+using FlightBooking.Lib.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Lib.Repositorio
+{
+    public class FuncionarioAviaoRepositorio : IFuncionarioAviaoRepository<IFuncionarioAviao>
+    {
+        private FlightBookingContext _ctxt;
+        private IConfiguration _config;
+        public FuncionarioAviaoRepositorio(IConfiguration config)
+        {
+            _config = config;
+            _ctxt = new FlightBookingContext(_config);
+        }
+        public void AlterarFuncionario(IFuncionarioAviao funcionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AlterarTempoDeVoo(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarFuncionario(IFuncionarioAviao funcionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFuncionarioAviao ObterDadosFuncionarioPorCPF(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFuncionarioAviao ObterDadosFuncionarioPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverFuncionario(IFuncionarioAviao funcionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<IFuncionarioAviao> RetornarTodosFuncionarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<IFuncionarioAviao> RetornarTodosOsCopilotos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<IFuncionarioAviao> RetornarTodosOsPilotos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
