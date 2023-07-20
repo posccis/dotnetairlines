016ecf8ccdbd6e1780620b8819244f0141bc2cb4
Posccis <lkzq33@gmail.com>
2023-07-20 01:51:25 -0300

    new(main)="Fiz as interfaces de repositorio e objeto para os funcionarios do avião"

---
+++ FlightBookingLib/Interfaces/IFuncionarioAviaoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Lib.Interfaces
+{
+    public interface IFuncionarioAviaoRepository<T> where T : IFuncionarioAviao
+    {
+        void CadastrarFuncionario( T funcionario);
+        T ObterDadosFuncionarioPorId(int id);
+        T ObterDadosFuncionarioPorCPF(string cpf);
+        void AlterarFuncionario(T funcionario);
+        void AlterarTempoDeVoo(string cpf);
+        void RemoverFuncionario(T funcionario);
+        ICollection<T> RetornarTodosFuncionarios();
+        ICollection<T> RetornarTodosOsPilotos();
+        ICollection<T> RetornarTodosOsCopilotos();
+    }
+}
