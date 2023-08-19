f7f3e22dcf58ad986c55999dc74960096d422a55
Posccis <lkzq33@gmail.com>
2023-08-19 02:11:54 -0300

    new(main): Created the Voo repository class and interface

---
+++ FlightBookingLib/Interfaces/IVooRepository.cs
@@ -0,0 +1,18 @@
+﻿using FlightBooking.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Repository.Interfaces
+{
+    public interface IVooRepository<T> where T : IVoo
+    {
+        void InserirVoo(T Voo);
+        void AlterarVoo(T Voo);
+        void RemoverVoo(T Voo);
+        List<T> ObterTodosOsVoos();
+        T ObterVooPorId(int Id);
+    }
+}
---
+++ FlightBookingLib/Repositorio/VooRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using FlightBooking.Domain.Models;
+using FlightBooking.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Repository.Repositorio
+{
+    public class VooRepositorio : IVooRepository<Voo>
+    {
+        public void AlterarVoo(Voo Voo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirVoo(Voo Voo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Voo> ObterTodosOsVoos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Voo ObterVooPorId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverVoo(Voo Voo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
