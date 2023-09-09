2145ce9184b54d019c3a6719b03621f55eaa95f0
Posccis <lkzq33@gmail.com>
2023-09-09 01:59:54 -0300

    new(main): Also created PassageiroValidacaoException

---
+++ FlightBooking.Service/Exceptions/PassageiroValidacaoException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Service.Exceptions
+{
+    public class PassageiroValidacaoException : Exception
+    {
+        public int Codigo { get; set; }
+        public PassageiroValidacaoException(int codigo, string message) : base(message)
+        {
+            Codigo = codigo;
+        }
+    }
+}
