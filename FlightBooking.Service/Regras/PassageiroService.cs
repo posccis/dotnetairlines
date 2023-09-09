b8a9de2d806b91a93b9447773b81c89c1c0ec60b
Posccis <lkzq33@gmail.com>
2023-09-09 01:58:01 -0300

    new(main): Created the PassageiroService class

---
+++ FlightBooking.Service/Regras/PassageiroService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Estudo_API_tesseract.Exceptions;
+using FlightBooking.Domain.Models;
+using FlightBooking.Repository.Repositorio;
+using FlightBooking.Service.Exceptions;
+using FlightBooking.Service.Validacoes;
+using Microsoft.Extensions.Configuration;
+
+namespace FlightBooking.Service.Regras
+{
+    public class PassageiroService
+    {
+        private IConfiguration _config;
+        private PassageiroRepositorio _passRepo;
+        public PassageiroValidacao passVal;
+        public PassageiroService() 
+        {
+            _config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _passRepo = new PassageiroRepositorio(_config);
+            passVal = new PassageiroValidacao();
+        }
+
+        public async Task CadastrarPassageiro(Passageiro passageiro) 
+        {
+            try
+            {
+                passVal.ValidarPassageiroInsercao(passageiro);
+                _passRepo.InserirPassageiro(passageiro);
+
+            }
+            catch (FlightBookingRepositoryException repoEx)
+            {
+
+                throw new PassageiroServiceException("Erro ao cadastrar passageiro! Mais detalhes:" + repoEx.Message);
+            }
+        }
+    }
+}
