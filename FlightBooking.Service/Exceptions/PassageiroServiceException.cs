db084f91f5a9561d1521d75fd8de720b18cc5faf
Posccis <lkzq33@gmail.com>
2023-09-09 01:59:17 -0300

    new(main): Created the PassageiroServiceException

---
+++ FlightBooking.Service/Exceptions/PassageiroServiceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Service.Exceptions
+{
+    public class PassageiroServiceException : Exception
+    {
+        public PassageiroServiceException( string message) : base(message)
+        {
+            
+        }
+    }
+}
