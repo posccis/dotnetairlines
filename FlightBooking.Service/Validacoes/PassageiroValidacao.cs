5380b40ff7705d29da7f273a60b2db913addb82d
Posccis <lkzq33@gmail.com>
2023-09-09 01:58:34 -0300

    new(main): Created the PassageiroValidacao class

---
+++ FlightBooking.Service/Validacoes/PassageiroValidacao.cs
@@ -0,0 +1,22 @@
+﻿using FlightBooking.Domain.Models;
+using FlightBooking.Service.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Service.Validacoes
+{
+    public class PassageiroValidacao
+    {
+        public PassageiroValidacao() { }
+
+        public void ValidarPassageiroInsercao(Passageiro passageiro) 
+        {
+            if (passageiro == null) throw new PassageiroValidacaoException(0, "O objeto do passageiro está nulo!");
+            if (string.IsNullOrEmpty(passageiro.Nome)) throw new PassageiroValidacaoException(1, "O nome do passageiro está vazio! É necessário informar um nome.");
+            if (string.IsNullOrEmpty(passageiro.CPF)) throw new PassageiroValidacaoException(3, "O CPF do passageiro está vazio! É necessário informar um CPF.");
+        }
+    }
+}
