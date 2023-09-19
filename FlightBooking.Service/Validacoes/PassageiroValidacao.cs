f8020c45fa3f93680f572a9965b8fd1485bd3ca6
Posccis <lkzq33@gmail.com>
2023-09-18 23:38:19 -0300

    update(main): Finished the first method of PassageiroValidacao

--- FlightBooking.Service/Validacoes/PassageiroValidacao.cs
+++ FlightBooking.Service/Validacoes/PassageiroValidacao.cs
@@ -2,6 +2,7 @@
 using FlightBooking.Service.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,15 @@ namespace FlightBooking.Service.Validacoes
         {
             if (passageiro == null) throw new PassageiroValidacaoException(0, "O objeto do passageiro está nulo!");
             if (string.IsNullOrEmpty(passageiro.Nome)) throw new PassageiroValidacaoException(1, "O nome do passageiro está vazio! É necessário informar um nome.");
-            if (string.IsNullOrEmpty(passageiro.CPF)) throw new PassageiroValidacaoException(3, "O CPF do passageiro está vazio! É necessário informar um CPF.");
+            if (string.IsNullOrEmpty(passageiro.CPF)) throw new PassageiroValidacaoException(2, "O CPF do passageiro está vazio! É necessário informar um CPF.");
+            if (!DateTime.TryParse(passageiro.DataNascimento, out var temp)) throw new PassageiroValidacaoException(3, "Data de nascimento invalida! É necessário informar um data de nascimento válida.");
+            if (String.IsNullOrEmpty(passageiro.Genero)) throw new PassageiroValidacaoException(4, "O genêro está vázio! É necessário informar algum valor ao campo.");
+            if (String.IsNullOrEmpty(passageiro.RG)) throw new PassageiroValidacaoException(5, "O RG do passageiro está vázio! É necessário informar um RG.");
+        }
+
+        public void ValidarRetornoTodosPassageiros(List<Passageiro> passageiros)
+        {
+
         }
     }
 }
