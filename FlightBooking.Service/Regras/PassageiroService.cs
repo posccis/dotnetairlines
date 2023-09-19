4d48677532af53442c762e7094784bbc1cd904b5
Posccis <lkzq33@gmail.com>
2023-09-18 23:37:39 -0300

    update(main): Made two new methods to the PassageiroService class

--- FlightBooking.Service/Regras/PassageiroService.cs
+++ FlightBooking.Service/Regras/PassageiroService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Estudo_API_tesseract.Exceptions;
 using FlightBooking.Domain.Models;
@@ -36,6 +37,39 @@ namespace FlightBooking.Service.Regras
             {
 
                 throw new PassageiroServiceException("Erro ao cadastrar passageiro! Mais detalhes:" + repoEx.Message);
+            }
+            catch(PassageiroValidacaoException passEx) 
+            {
+                if (passEx.Codigo == 0) throw new PassageiroServiceException("Tente novamente a inserção e certifique de que preencheu os campos antes de enviar!" + passEx.Message);
+                else if (passEx.Codigo == 1) throw new PassageiroServiceException("O nome do passageiro é um campo obrigatório! Preencha e envie novamente." + passEx.Message);
+                else if (passEx.Codigo == 2) throw new PassageiroServiceException("O CPF do passageiro é um campo obrigatório! Preencha e envie novamente." + passEx.Message);
+                else if (passEx.Codigo == 3) throw new PassageiroServiceException("A data de nascimento informada não é válida! Corrija e envie novamente." + passEx.Message);
+                else if (passEx.Codigo == 4) throw new PassageiroServiceException("O campo gênero não foi preenchido! Escolha uma das opções e envie novamnete." + passEx.Message);
+                else if (passEx.Codigo == 5) throw new PassageiroServiceException("O RG do passageiro é um campo obrigatório! Preencha e envie novamente." + passEx.Message);
+            }
+        }
+        public async Task RetornaTodosPassageiros(Passageiro passageiro) 
+        {
+            try
+            {
+                await _passRepo.RetornarTodosPassageiros();
+                passVal.ValidarPassageiroInsercao(passageiro);
+
+
+            }
+            catch (FlightBookingRepositoryException repoEx)
+            {
+
+                throw new PassageiroServiceException("Erro ao cadastrar passageiro! Mais detalhes:" + repoEx.Message);
+            }
+            catch(PassageiroValidacaoException passEx) 
+            {
+                if (passEx.Codigo == 0) throw new PassageiroServiceException("Tente novamente a inserção e certifique de que preencheu os campos antes de enviar!" + passEx.Message);
+                else if (passEx.Codigo == 1) throw new PassageiroServiceException("O nome do passageiro é um campo obrigatório! Preencha e envie novamente." + passEx.Message);
+                else if (passEx.Codigo == 2) throw new PassageiroServiceException("O CPF do passageiro é um campo obrigatório! Preencha e envie novamente." + passEx.Message);
+                else if (passEx.Codigo == 3) throw new PassageiroServiceException("A data de nascimento informada não é válida! Corrija e envie novamente." + passEx.Message);
+                else if (passEx.Codigo == 4) throw new PassageiroServiceException("O campo gênero não foi preenchido! Escolha uma das opções e envie novamnete." + passEx.Message);
+                else if (passEx.Codigo == 5) throw new PassageiroServiceException("O RG do passageiro é um campo obrigatório! Preencha e envie novamente." + passEx.Message);
             }
         }
     }
